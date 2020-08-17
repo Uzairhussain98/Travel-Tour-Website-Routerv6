ffa2470568cc82cd6dd960250fa3fbd6474fc65b
Uzair Hussain <uzairhussain98@hotmail.com>
2020-08-18 01:51:51 +0500

    started travel website routerv6

---
+++ src/components/Home/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Home.css';
+import Typography from '@material-ui/core/Typography';
+
+
+
+const Home = () => {
+  return (
+    <div className="home">
+        <div className="main">
+            <h1 style={{color: "#8CC2F9"  , fontSize : "50px"}}>Welcome To Travel And Tourers</h1>
+           <h3 className="maintxt"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+           when an unknown printer took a galley of type and scrambled it to make a type specimen book.</h3>
+
+
+            </div>
+      
+    </div>
+  )
+}
+
+export default Home
